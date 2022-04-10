b64b48d4e2f377d812873b4afb17a2d3de00a929
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-10 08:07:32 +0100

    contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -18,6 +18,14 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
+
+    function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
 
 }
